65a652923176394663e751dd4528bfbf2f5e131a
Christopher Sykes <justchrissykes@gmail.com>
2021-01-17 15:47:14 -0500

    More fudge
    getting there

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 const app = express();
-app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/", express.static(path.join(__dirname, "../trapp/out")));
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`);
 });
