700200859c053462a554f1784842f214c4258dff
danibarker <caseybarker86@gmail.com>
2021-02-15 17:37:48 -0700

    Revert "Refactoring into separate files."
    This reverts commit f9e0fc76630cefe9d957aba39b94bece86afffe3.

--- trapp/pages/pb/Components/RackTile.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const RackTile = (props) => {
-  return (
-    <span className={props.tileclass} onClick={props.onClick}>
-      {props.tilevalue}
-    </span>
-  );
-};
